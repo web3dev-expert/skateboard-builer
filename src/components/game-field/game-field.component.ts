612cdcffa27b3a4c67bd73cb4fec8cefe72e319d
web3dev-expert <astorjonsson97@proton.me>
2024-12-01 12:56:15 +0100

    generated components

---
+++ src/components/game-field/game-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-field.component.html',
+  styleUrl: './game-field.component.scss'
+})
+export class GameFieldComponent {
+
+}
