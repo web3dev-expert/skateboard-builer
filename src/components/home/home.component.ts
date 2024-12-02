f824568ddfa72a1d7b523269342928a81b4f5366
web3dev-expert <astorjonsson97@proton.me>
2024-12-02 22:02:27 +0100

    doing home

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgIf,NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    var  svg = document.getElementById("svg")! as unknown as SVGGraphicsElement;
+    var  bbox = svg.getBBox();
+    svg.setAttribute("width", String(bbox.x + bbox.width + bbox.x));
+//     svg.setAttribute("height",  String(bbox.y + bbox.height + bbox.y)); 
+ }
 }
