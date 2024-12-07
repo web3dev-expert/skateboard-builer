f70f392c1d1f53e8b7485f1ce69ead9c5e2bdc05
web3dev-expert <astorjonsson97@proton.me>
2024-12-07 20:00:18 +0100

    done some changes in home component

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -26,14 +26,15 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
 
     this.interval = setInterval(() => {
       let starContainer = document.querySelector('.star-container');
-
+      starContainer?.classList.add('py-5')
       let img = document.createElement('img') as HTMLImageElement;
       img.src = 'assets/home/star.png';
       img.style.width = '50px';
       img.style.height = '50px';
-
+      img.classList.add('m-auto')
       if (this.starCount == 5) {
         starContainer!.innerHTML = ''
+        this.starCount=0;
       }
       this.starCount++;
       starContainer?.append(img);
