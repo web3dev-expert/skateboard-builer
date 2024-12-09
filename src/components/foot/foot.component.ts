b35ceeb2788007edab7a24dd3f455b5298418eed
web3dev-expert <astorjonsson97@proton.me>
2024-12-09 10:44:12 +0100

    doing footer

--- src/components/foot/foot.component.ts
+++ src/components/foot/foot.component.ts
@@ -1,12 +1,20 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { ModeService } from '../../services/mode.service';
 
 @Component({
   selector: 'app-foot',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './foot.component.html',
   styleUrl: './foot.component.scss'
 })
 export class FootComponent {
+  mode: string = 'light';
 
+  constructor(private modeService:ModeService){
+    this.modeService.mode.subscribe((data:string)=>{
+      this.mode=data;
+    })
+  }
 }
