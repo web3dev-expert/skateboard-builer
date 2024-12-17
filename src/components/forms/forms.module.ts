54082d73bad3ce6afbff7d1f42a2290ee24e32bc
web3dev-expert <astorjonsson97@proton.me>
2024-12-17 02:07:29 +0100

    declared login and signup component in forms module

--- src/components/forms/forms.module.ts
+++ src/components/forms/forms.module.ts
@@ -3,16 +3,23 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { FormsComponent } from "./forms.component";
 import { CommonModule } from "@angular/common";
 import { FormsRoutingModule } from "./forms-routing.module";
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
 
 @NgModule({
       declarations: [
-        FormsComponent
+        FormsComponent,
+        LoginComponent,
+        SignupComponent
       ],
       imports: [
         CommonModule,
         ReactiveFormsModule,
         FormsRoutingModule
       ],
-      
+      exports: [
+        LoginComponent,
+        SignupComponent
+      ]
     })
 export class FormsModule {}
