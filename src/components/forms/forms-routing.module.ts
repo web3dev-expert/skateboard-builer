1738f4b7ad7aef116cffcef55d69d88f4f04d950
web3dev-expert <astorjonsson97@proton.me>
2024-12-20 11:55:42 +0100

    fixed route problems

--- src/components/forms/forms-routing.module.ts
+++ src/components/forms/forms-routing.module.ts
@@ -17,6 +17,10 @@ export const routes: Routes = [
     {
         path: 'forms/signup',
         component: SignupComponent
+    },
+    {
+        path: '**',
+        component: SignupComponent
     }
 ];
 @NgModule({
