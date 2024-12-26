5a0f48c4911eff99710e23b7608b1d590d719810
web3dev-expert <astorjonsson97@proton.me>
2024-12-26 21:51:26 +0100

    soing login and signup

--- src/components/forms/components/login/login.component.ts
+++ src/components/forms/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrIconClasses, ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -12,14 +13,13 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({});
   isLoginFormSubmitted: boolean = false;
 
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService,private router:Router) { }
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
-    console.log('login')
   }
 
   login() {
@@ -30,4 +30,7 @@ export class LoginComponent implements OnInit {
     }
   }
 
+goToSignup(route:string){
+  this.router.navigate([`forms/${route}`])
+}
 }
