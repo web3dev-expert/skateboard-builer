0a79ed02a4d53cabf5ef12360ce6c6b282c49ec9
web3dev-expert <astorjonsson97@proton.me>
2025-01-24 18:41:23 +0100

    reached a nice animation

--- src/components/forms/forms.component.ts
+++ src/components/forms/forms.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup} from '@angular/forms';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormsModule } from './forms.module';
 import { ModeService } from '../../services/mode.service';
@@ -9,22 +9,28 @@ import { ModeService } from '../../services/mode.service';
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
-export class FormsComponent implements OnInit{
-login:FormGroup = new FormGroup({});
-signup:FormGroup = new FormGroup({});
-mode:string = 'light';
-balls:number[]=[1,2,3,4,5,6]
-constructor(private router:Router,private modeService:ModeService){
-  this.modeService.mode.subscribe((data:string)=>{
-    if(data){
-      this.mode=data;
-    }
-  })
-}
-ngOnInit(): void {
-}
+export class FormsComponent implements OnInit {
+  login: FormGroup = new FormGroup({});
+  signup: FormGroup = new FormGroup({});
+  mode: string = 'light';
+  balls: number[] = [1, 2, 3, 4, 5, 6]
+  window: { innerWidth: number } = { innerWidth: 0 }
+  constructor(private router: Router, private modeService: ModeService) {
+    this.modeService.mode.subscribe((data: string) => {
+      if (data) {
+        this.mode = data;
+      }
+    })
+  }
+  ngOnInit(): void {
+    this.window=window;
+  }
 
-route(direction:string){
-  this.router.navigate([`forms/${direction}`])
-}
+  route(direction: string) {
+    this.router.navigate([`forms/${direction}`])
+  }
+  @HostListener('window:resize',['event'])
+  onResize(){
+    this.window=window;
+  }
 }
