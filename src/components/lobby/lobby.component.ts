8b82f16528be3832c672cd6328ac6d4e3dd61429
web3dev-expert <astorjonsson97@proton.me>
2025-02-04 22:15:38 +0100

    doing lobby

--- src/components/lobby/lobby.component.ts
+++ src/components/lobby/lobby.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interfaces/interfaces';
+import { NgClass, NgIf } from '@angular/common';
+import { ModeService } from '../../services/mode.service';
 
 @Component({
   selector: 'app-lobby',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgIf],
   templateUrl: './lobby.component.html',
   styleUrl: './lobby.component.scss'
 })
-export class LobbyComponent implements OnInit{
+export class LobbyComponent implements OnInit {
+
+  user: User | null;
+  toggle: boolean = false;
+  mode: string = 'light';
+  constructor(private authService: AuthService, private modeService: ModeService) {
+    this.user = this.authService.getUser()
+    this.modeService.mode.subscribe((data: string) => {
+      this.mode = data;
+    })
+  }
 
   ngOnInit(): void {
-    localStorage.setItem('location','lobby')
+    localStorage.setItem('location', 'lobby')
   }
 }
