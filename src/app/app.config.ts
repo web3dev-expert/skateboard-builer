c67aa3c565eff126b0a8c0674afd74c50a643fb0
web3dev-expert <astorjonsson97@proton.me>
2025-03-06 01:13:59 +0100

    angular material is not working

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,13 +8,15 @@ import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@a
 import { AuthInterceptor } from '../core/auth.interceptor';
 import { ErrorInterceptor } from '../core/error.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
 
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(), 
           provideToastr(),provideHttpClient(
             withInterceptorsFromDi()
-          ),
+          ), provideNativeDateAdapter(),
+          
           { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
           { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }, provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()
           ],
--- src/components/lobby/lobby.component.ts
+++ src/components/lobby/lobby.component.ts
@@ -1,15 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../interfaces/interfaces';
-import { NgClass, NgIf } from '@angular/common';
 import { ModeService } from '../../services/mode.service';
-import { GiochiComponent } from '../giochi/giochi.component';
-import { ClassificheComponent } from '../classifiche/classifiche.component';
-import { TrofeiComponent } from '../trofei/trofei.component';
-import { PunteggiComponent } from '../punteggi/punteggi.component';
-import { StatisticheComponent } from '../statistiche/statistiche.component';
-import { AboutUsComponent } from '../about-us/about-us.component';
-import { AssistenzaComponent } from '../assistenza/assistenza.component';
 
 @Component({
   selector: 'app-lobby',
