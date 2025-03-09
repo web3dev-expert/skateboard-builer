c508d4a8b5db185479684d1764c98a4da331dc68
web3dev-expert <astorjonsson97@proton.me>
2025-03-09 02:19:04 +0100

    fiu

--- src/components/giochi/giochi.component.ts
+++ src/components/giochi/giochi.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, Sanitizer } from '@angular/core';
 import { GiochiService } from '../../services/giochi.service';
 import { throttleTime } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { GiocoPreviewComponent } from '../../shared/components/gioco-preview/gioco-preview.component';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-giochi',
@@ -15,54 +16,72 @@ export class GiochiComponent implements OnInit {
   giochi: any[] = [];
   circles: number[] = [1, 2, 3, 4, 5];
   searchGiocoForm: FormGroup = new FormGroup({});
-  points:number[] = [1,2,3,4];
-  difficulties:number[] = [1,2,3,4,5];
-  constructor(private giochiService: GiochiService, private matDialog: MatDialog, private toastr:ToastrService) { }
+  points: number[] = [1, 2, 3, 4];
+  difficulties: number[] = [1, 2, 3, 4];
+  page: number = 0;
+  size: number = 1;
+  orderBy: string = "id";
+  sortOrder: string = "ASC";
+  body: { nome: string, difficolta: number, punteggio: number } = {
+    nome: this.searchGiocoForm.get('nomeGioco')?.value,
+    difficolta: this.searchGiocoForm.get('difficolta')?.value,
+    punteggio: this.searchGiocoForm.get('punteggioRecensioniDa')?.value
+  };
+  isLoading:boolean = false;
+  constructor(private giochiService: GiochiService, private matDialog: MatDialog, private changeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    this.getGiochi();
-    this.initializeGiocoForm()
+    this.initializeGiocoForm();
+    this.getGiochi(this.body);
   }
 
-  getGiochi() {
-    this.giochiService.getGiochi().pipe(throttleTime(1000)).subscribe({
+  getGiochi(body: { nome: string, difficolta: number, punteggio: number }, isActive?: boolean) {
+    this.giochiService.searchGiochi(body, this.page, this.size, this.orderBy, this.sortOrder, isActive).pipe(throttleTime(1000)).subscribe({
       next: (data: any) => {
-        this.giochi = data;
+        console.log(data.content)
+        data?.content?.map((g: any) => { this.giochi.push(g) })
+        console.log(this.giochi)
         this.giochi.filter((g: any) => {
           g.image = this.readGiocoImage(g?.image)
         })
+        this.isLoading = false;
       }
     })
   }
   readGiocoImage(image: any) {
+    if (image.startsWith("data:image/png;base64,")) return image;
     return "data:image/png;base64," + image;
   }
   openGameDialog(gioco: any) {
-    const dialogRef = this.matDialog.open(GiocoPreviewComponent,{
-      data:gioco,
+    const dialogRef = this.matDialog.open(GiocoPreviewComponent, {
+      data: gioco,
       width: '50%',
-      height: '70%'})
-    dialogRef.afterClosed().subscribe((data:any)=>{})
+      height: '70%'
+    })
+    dialogRef.afterClosed().subscribe((data: any) => { })
   }
 
-  initializeGiocoForm(){
+  initializeGiocoForm() {
     this.searchGiocoForm = new FormGroup({
       nomeGioco: new FormControl(''),
-      difficolta: new FormControl(''),
+      difficolta: new FormControl(),
       punteggioRecensioniDa: new FormControl('')
     })
   }
 
-  searchGiochi(){
-    let body:{nome:string,difficolta:number,punteggio:number} = {
+  searchGiochi() {
+    this.body = {
       nome: this.searchGiocoForm.get('nomeGioco')?.value,
       difficolta: this.searchGiocoForm.get('difficolta')?.value,
       punteggio: this.searchGiocoForm.get('punteggioRecensioniDa')?.value
+    };
+    this.getGiochi(this.body);
+  }
+  onScroll(event: any) {
+    if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight-5 && !this.isLoading) {
+      this.page += 1;
+      this.isLoading = true;
+      this.searchGiochi();
     }
-      this.giochiService.searchGiochi(body).subscribe({
-        next:(data:any)=>{
-          console.log(data)
-        }
-      })
   }
 }
--- src/services/giochi.service.ts
+++ src/services/giochi.service.ts
@@ -12,11 +12,10 @@ export class GiochiService{
 
 constructor(private http:HttpClient){}
 
-getGiochi(isActive?:boolean){
-    return this.http.get(environment.API_URL + this.gioco+`?isActive=${isActive||true}`);
-}
 
-searchGiochi(body:{nome:string,difficolta:number,punteggio:number}){
-    return this.http.get(environment.API_URL + this.gioco + this.byFilters + `?nomeGioco=${body?.nome||""}&difficolta=${body?.difficolta||0}&avg=${body?.punteggio||1}`);
+
+searchGiochi(body:{nome:string,difficolta:number,punteggio:number},page:number,size:number,orderBy:string,sortOrderd:string,isActive?:boolean){
+    return this.http.get(environment.API_URL + this.gioco + this.byFilters + 
+        `?nomeGioco=${body?.nome||""}&difficolta=${body?.difficolta||0}&avg=${body?.punteggio||0}&page=${page}&size=${size}&orderBy=${orderBy}&sortOrder=${sortOrderd}&isActive=${isActive}`);
 }
 };
