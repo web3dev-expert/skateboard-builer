bd88e75de995afe78965bcd249ee8aa8bfcd50f9
web3dev-expert <astorjonsson97@proton.me>
2025-03-18 01:34:33 +0100

    doing profilo

--- src/shared/components/recensioni/recensioni.component.ts
+++ src/shared/components/recensioni/recensioni.component.ts
@@ -7,11 +7,12 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AskConfirmComponent } from '../ask-confirm/ask-confirm.component';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-recensioni',
   standalone: true,
-  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, NgClass, NgFor, MatDialogClose, NgIf, ReactiveFormsModule],
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatMenuModule, NgClass, NgFor, MatDialogClose, NgIf, ReactiveFormsModule],
   templateUrl: './recensioni.component.html',
   styleUrl: './recensioni.component.scss'
 })
@@ -64,7 +65,7 @@ export class RecensioniComponent implements OnInit {
         punteggio: this.recensioneForm.get('punteggio')?.value
       }
 
-      const dialogRef = this.dialog.open(AskConfirmComponent, { data: [this.gioco?.nomeGioco, recensione], width: '60%', height: '70%' })
+      const dialogRef = this.dialog.open(AskConfirmComponent, { data: [this.gioco?.nomeGioco, recensione, ''], width: '60%', height: '70%' })
 
       dialogRef.afterClosed().subscribe((data: boolean) => {
         if (data) {
@@ -86,7 +87,39 @@ export class RecensioniComponent implements OnInit {
     return (this.recensioneForm.get('commento')!.invalid || this.recensioneForm.get('commento')!.value.trim() === "")
   }
 
-  toNumber(value:string){
+  toNumber(value: string) {
     return Number(value);
+  }
+
+  updateRece(recensione: any) {
+    const dialogRef = this.dialog.open(AskConfirmComponent, { data: [this.gioco?.nomeGioco, recensione, ''], width: '60%', height: '70%' })
+
+    dialogRef.afterClosed().subscribe((data: boolean) => {
+      if (data) {
+        this.recensioneService.saveRecensione(recensione).subscribe({
+          next: () => {
+            this.getRecensioni();
+          }
+        })
+      } else {
+        this.toastr.show("Non è stata aggiunta nessuna recensione.")
+      }
+    })
+  }
+
+  deleteRece(recensione: any) {
+    const dialogRef = this.dialog.open(AskConfirmComponent, { data: [this.gioco?.nomeGioco, recensione, ''], width: '60%', height: '70%' })
+
+    dialogRef.afterClosed().subscribe((data: boolean) => {
+      if (data) {
+        this.recensioneService.saveRecensione(recensione).subscribe({
+          next: () => {
+            this.getRecensioni();
+          }
+        })
+      } else {
+        this.toastr.show("Non è stata aggiunta nessuna recensione.")
+      }
+    })
   }
 }
