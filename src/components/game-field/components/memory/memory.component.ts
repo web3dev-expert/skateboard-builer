0f955182b9c38b737a0071e653eff107d8c279fc
web3dev-expert <astorjonsson97@proton.me>
2025-04-04 01:32:03 +0200

    doing memory

--- src/components/game-field/components/memory/memory.component.ts
+++ src/components/game-field/components/memory/memory.component.ts
@@ -1,24 +1,33 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../../../../interfaces/interfaces';
 import { AuthService } from '../../../../services/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-memory',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './memory.component.html',
   styleUrl: './memory.component.scss'
 })
 export class MemoryComponent implements OnInit {
 
-  @Input() game!: number
+  @Input() game!: number;
   user: User | null = null;
-
-  constructor(private authService: AuthService) { 
+  step: number = 0;
+  difficolta: string = '';
+  constructor(private authService: AuthService) {
     this.user = this.authService.getUser();
   }
 
   ngOnInit(): void {
+    this.step = 1
+  }
+
+
+  go(value?: string) {
+    this.difficolta = value || this.difficolta;
+    this.step++
   }
 
 }
