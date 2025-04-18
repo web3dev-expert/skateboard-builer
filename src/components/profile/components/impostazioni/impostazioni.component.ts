3318bd6a3b1520d99cf6e483eea6b45ce5325ff4
web3dev-expert <astorjonsson97@proton.me>
2025-04-18 22:30:24 +0200

    doing assistenza in profilo

--- src/components/profile/components/impostazioni/impostazioni.component.ts
+++ src/components/profile/components/impostazioni/impostazioni.component.ts
@@ -1,20 +1,35 @@
 import { NgIf } from '@angular/common';
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { User } from '../../../../interfaces/interfaces';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-impostazioni',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, ReactiveFormsModule,MatTooltipModule],
   templateUrl: './impostazioni.component.html',
   styleUrl: './impostazioni.component.scss'
 })
-export class ImpostazioniComponent implements OnInit, OnChanges{
-@Input() section: string = '';
+export class ImpostazioniComponent implements OnInit {
+  @Input() section: string = '';
+  @Input() user: User | null = null;
+  assistenzaForm: FormGroup = new FormGroup({});
 
-ngOnChanges(changes: SimpleChanges): void {
-  console.log(this.section);
-}
-ngOnInit(): void {
-  
-}
+  constructor(private toastr: ToastrService) { }
+  ngOnInit(): void {
+    this.assistenzaForm = new FormGroup({
+      oggetto: new FormControl('', Validators.required),
+      descrizione: new FormControl('', Validators.required)
+    })
+  }
+
+  inviaRichiesta() {
+    if (this.assistenzaForm.valid) {
+
+    } else {
+      this.toastr.show('Inserisci i campi richiesti.')
+    }
+  }
 }
