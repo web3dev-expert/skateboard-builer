fd95593cfe49ac5bc26d99e78439ae00ddea028f
web3dev-expert <astorjonsson97@proton.me>
2025-05-04 02:45:21 +0200

    doing text-editor

--- src/shared/components/text-editor/text-editor.component.ts
+++ src/shared/components/text-editor/text-editor.component.ts
@@ -1,12 +1,35 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-text-editor',
   standalone: true,
-  imports: [],
+  imports: [NgClass,NgFor],
   templateUrl: './text-editor.component.html',
   styleUrl: './text-editor.component.scss'
 })
 export class TextEditorComponent {
+  icons: Set<string> = new Set([
+    'bi-type-bold',
+    'bi-type-italic',
+    'bi-type-underline',
+    'bi-type-strikethrough',
+    'bi-text-center',
+    'bi-text-left',
+    'bi-text-right'
+  ]);
+  selectedItems: string = '';
+  point(event: any) {
+    console.log(event)
+  }
 
+  selectItems(items: string) {
+    console.log(this.selectedItems , items)
+    if (this.selectedItems == items) {
+      this.selectedItems = ''
+    } else {
+      this.selectedItems = items
+    }
+  }
+ 
 }
